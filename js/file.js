8bb78d0798fe488056d4164cfaae656cff1c44f9
RehamRaafat <marwan.mahmoud.fcis@outlook.com>
2018-11-01 20:09:06 +0200

    final

--- js/file.js
+++ js/file.js
@@ -15,5 +15,5 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 13000); // Change image every 5 seconds
+    setTimeout(showSlides, 13000); // Change image every 13 seconds
 }
